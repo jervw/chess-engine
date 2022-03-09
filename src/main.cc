39dbf7c2d635605b46a8a43fe009aa51faf0d1c5
jervw <vuolajere@gmail.com>
2022-03-09 17:26:08 +0200

    added tile piece values and added arguments

--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,40 @@
+#include <string.h>
 #include "game.hh"
 #include "human.hh"
 #include "engine.hh"
 
 const bool WHITE = true, BLACK = false;
 
-int main() {
+int main(int argc, char** argv) {
     Board* board = new Board();
     Player* white; Player* black;
 
-    int depth;
-    std::cout << "depth: ";
-    std::cin >> depth;
+    if (argc > 2) {
+        int depth = atoi(argv[2]);
+        if (strcmp(argv[1], "--white") == 0) {
+            white = new Human(WHITE);
+            black = new Engine(BLACK, depth);
+        }
+        else if (strcmp(argv[1], "--black") == 0) {
+            white = new Engine(WHITE, depth);
+            black = new Human(BLACK);
+        }
+        else if (strcmp(argv[1], "--engine") == 0) {
+            white = new Engine(WHITE, depth);
+            black = new Engine(BLACK, depth);
+        }
+        else {
+            std::cerr << "Invalid arguments, use --white, --black or --engine followed by depth" << std::endl;
+            return 1;
+        }
 
-    white = new Human(WHITE);
-    black = new Engine(BLACK, depth);
-
-    Game game(board, white, black);
-    game.play();
-    return 0;
+        // Start game
+        Game game(board, white, black);
+        game.play();
+        return 0;
+    }
+    else {
+        std::cerr << "Invalid arguments, use --white, --black or --engine followed by depth" << std::endl;
+        return 1;
+    }
 }
